d42443a46442ddfd995c66fe5f4701eb8e4f6346
gangfunction <gangfunction@gmail.com>
2024-06-24 16:18:32 +0900

    git init

---
+++ src/main/java/com/tddcleanarchi/TddCleanArchiApplication.java
@@ -0,0 +1,13 @@
+package com.tddcleanarchi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddCleanArchiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TddCleanArchiApplication.class, args);
+    }
+
+}
